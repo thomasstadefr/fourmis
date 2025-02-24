5dbadfeb9a50ce8b42c19ba0d6172235abce7317
thomasstadefr <echecthomas@gmail.com>
2025-02-24 17:38:50 +0100

    Création de la classe Genetique

---
+++ colonie.py
@@ -0,0 +1,9 @@
+from fourmi import Fourmi
+
+class Colonie:
+    def __init__(self):
+        pass
+
+    def evaporation(self):
+        pass
+
--- genetique.py
+++ genetique.py
@@ -1,2 +1,14 @@
-def test(): 
-    pass
+from fourmi import Fourmi
+
+class Genetique:
+    def __init__(self):
+        pass
+
+    def mutation(self):
+        pass
+
+    def crossover(self):
+        pass
+
+    def reproduction(self):
+        pass
